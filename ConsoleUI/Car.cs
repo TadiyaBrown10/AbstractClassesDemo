3f04675ed1010fe83fafc4529fde4da2a67488a5
Tadiya Brown <tadiyabrown2000@gmail.com>
2023-04-24 20:41:00 -0400

    abstract work complete

---
+++ ConsoleUI/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ConsoleUI
+{
+	public class Car : Vehicle
+	{
+		
+
+        public override void DriveAbstract()
+        {
+            Console.WriteLine("This car is on the road.");
+        }
+
+        public bool Hastrunk { get; set; } = true;
+    }
+}
+
---
+++ ConsoleUI/Motorcycle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ConsoleUI
+{
+	public class Motorcycle : Vehicle
+	{
+
+        public override void DriveAbstract()
+        {
+            Console.WriteLine("This Motorcycle is on the move.");
+        }
+
+        public bool HasSideCart { get; set; } = true;
+
+        public override void DriveVirtual()
+        {
+            Console.WriteLine("The Motorcycle takes to the streets.");
+        }
+
+    }
+}
+
